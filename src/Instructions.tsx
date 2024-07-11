369b7d037da87a38c7617350d2543111761c35c7
jaqbek.eth <jaqbek.eth@gmail.com>
2024-07-11 23:17:15 +0200

    049

--- src/Instructions.tsx
+++ src/Instructions.tsx
@@ -33,7 +33,7 @@ const Instructions: React.FC<InstructionsProps> = ({ onStartGame }) => (
       <ol style={{ fontSize: '0.6rem' }}> {/* Smaller list */}
         <li>Click on the car image to move forward.</li>
         <li>Every 10 clicks will reveal the gear that disables clicking on the car.</li>
-        <li>Click on the gear image to activate further car clicks .</li>
+        <li>Click on the gear image to activate further car clicks.</li>
         <li>Your goal is to reach 69 clicks as fast as possible.</li>
       </ol>
       <p style={{ fontSize: '0.9rem' }}>When you're ready click</p> {/* Slightly larger paragraph */}
