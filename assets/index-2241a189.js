7f7ff4aad354990499b195e10d2baf9231ff1d2b
0xjaqbek <jaqbek.eth@gmail.com>
2024-07-14 23:14:02 +0000

    Deploying to gh-pages from @ 0xjaqbek/twa@085ef7903fa82329e2ff2ad32b70f66d9c5a90b2 🚀

--- assets/index-2241a189.js
+++ assets/index-2241a189.js
@@ -1164,8 +1164,8 @@ PERFORMANCE OF THIS SOFTWARE.
   color: var(--primary-text-color);
   border: 2px solid white;
   border-radius: 10px;
-  padding: 20px 30px;
-  font-size: 16px;
+  padding: 10px 20px;
+  font-size: 14px;
   cursor: pointer;
   transition: background-color 0.3s ease, box-shadow 0.3s ease;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
