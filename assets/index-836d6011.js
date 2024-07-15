98128a2fb6cda44d9756e51b94fd22a78be3819e
0xjaqbek <jaqbek.eth@gmail.com>
2024-07-15 20:43:33 +0000

    Deploying to gh-pages from @ 0xjaqbek/twa@9f8a2a69d276a8fe4d762b5496097a034510a7df 🚀

--- assets/index-836d6011.js
+++ assets/index-836d6011.js
@@ -2814,7 +2814,7 @@ FIREBASE: `))}restoreState_(){this.tryAuth(),this.tryAppCheck();for(const e of t
 `,cG=on.li`
   font-size: 13px;
   color: white;
-  margin: 10px 0;
+  margin: 5px 0;
   padding: 10px;
   background-color: black;
   border: 1px solid #ddd;
