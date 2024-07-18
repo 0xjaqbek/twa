bba78c74afe173185a241111801f2596c897048a
0xjaqbek <jaqbek.eth@gmail.com>
2024-07-18 08:50:47 +0200

    6124

--- src/LeaderboardPage.tsx
+++ src/LeaderboardPage.tsx
@@ -115,7 +115,6 @@ const LeaderboardPage: React.FC<LeaderboardPageProps> = ({ elapsedTime, onClose,
   const [onTelegram, setOnTelegram] = useState(false);
   const [userName, setUserName] = useState<string>('');
   const [lastName, setLastName] = useState<string>('');
-  const [userIdState, setUserId] = useState<string | null>(userId); // Correctly define setUserId
 
   const itemsPerPage = 3;
 
@@ -133,23 +132,23 @@ const LeaderboardPage: React.FC<LeaderboardPageProps> = ({ elapsedTime, onClose,
 
   const handleSaveScoreConfirm = async () => {
     try {
-      if (!userIdState) {
+      if (!userId) {
         alert("Please open in Telegram App.");
         return;
       }
-
+  
       const newScore: LeaderboardEntry = {
-        address: rawAddress || userName || firstName, // Use userName or firstName if address is empty
+        address: rawAddress || '',
         time: elapsedTime,
-        playerId: userIdState || '',
+        playerId: userId || '',
         userName: userName || firstName, // Use userName or firstName if userName is not available
       };
-
-      const existingScore = leaderboard.find(score => score.address === rawAddress || score.playerId === userIdState);
+  
+      const existingScore = leaderboard.find(score => score.address === rawAddress || score.playerId === userId);
       if (existingScore) {
         if (elapsedTime < existingScore.time) {
           const updatedLeaderboard = leaderboard.map(score =>
-            (score.address === rawAddress || score.playerId === userIdState) ? newScore : score
+            (score.address === rawAddress || score.playerId === userId) ? newScore : score
           );
           setLeaderboard(updatedLeaderboard);
           await updateLeaderboard(updatedLeaderboard);
@@ -173,84 +172,81 @@ const LeaderboardPage: React.FC<LeaderboardPageProps> = ({ elapsedTime, onClose,
     }
   };
 
-  const handleSaveScoreCancel = () => {
-    setShowSaveScoreWindow(false);
+  const getTopScores = (scores: LeaderboardEntry[]) => {
+    const highestScores = scores.reduce((acc, score) => {
+      if (!acc[score.address || score.playerId] || acc[score.address || score.playerId].time > score.time) {
+        acc[score.address || score.playerId] = score;
+      }
+      return acc;
+    }, {} as Record<string, LeaderboardEntry>);
+
+    const sortedScores = Object.values(highestScores).sort((a, b) => a.time - b.time);
+    return sortedScores;
   };
 
   const formatAddress = (address: string, userName: string, firstName: string) => {
-    const displayName = address || userName || firstName;
-    if (displayName.length <= 9) return displayName;
-    return `${displayName.slice(0, 5)}...${displayName.slice(-4)}`;
+    if (address.length === 0) {
+      return userName || firstName || 'Unknown'; // Display userName, firstName, or a fallback if both are empty
+    }
+    if (address.length <= 9) return address;
+    return `${address.slice(0, 5)}...${address.slice(-4)}`;
   };
 
-  const tg = (window as any).Telegram?.WebApp;
+  const handleNextPage = () => {
+    setPageIndex(prevPageIndex => prevPageIndex + 1);
+  };
 
-  if (tg) {
-    tg.ready(); // Ensure that Telegram Web App is fully loaded
-    const searchParams = new URLSearchParams(tg.initData);
+  const handlePrevPage = () => {
+    setPageIndex(prevPageIndex => Math.max(prevPageIndex - 1, 0));
+  };
 
-    const user = searchParams.get('user');
-    if (user) {
-      const userObj = JSON.parse(user);
-      setOnTelegram(true);
-      setUserId(userObj.id);
-      setFirstName(userObj.first_name || null);
-      setLastName(userObj.last_name || null);
-      setUserName(userObj.username || null);
-    } else {
-      setOnTelegram(false);
-      setUserId(null); // Set userId to null if user is undefined
-      setFirstName('');
-      setUserName('');
-      setLastName('');
-    }
-  } else {
-    setOnTelegram(false);
-    setUserId(null); // Set userId to null if Telegram WebApp is not loaded
-    setFirstName('');
-    setUserName('');
-    setLastName('');
-  }
+  const handleCloseSaveScoreWindow = () => {
+    setShowSaveScoreWindow(false);
+  };
+
+  const topScores = getTopScores(leaderboard);
+  const paginatedScores = topScores.slice(pageIndex * itemsPerPage, (pageIndex + 1) * itemsPerPage);
 
   return (
     <LeaderboardContainer>
       <LeaderboardContent>
-        <ElapsedTime>Your Time: {elapsedTime.toFixed(2)} seconds</ElapsedTime>
+        {elapsedTime > 0 && (
+          <>
+            <ElapsedTime style={{ marginBottom: '20px' }}>Your current time:<br></br>{elapsedTime.toFixed(2)} seconds</ElapsedTime>
+            <ActionsContainer>
+              <StyledButton onClick={handleSaveScore} style={{ marginBottom: '10px' }}>Save Score</StyledButton>
+            </ActionsContainer>
+          </>
+        )}
+        <h1 style={{ color: 'white' }}>Top</h1>
         <ActionsContainer>
-          {rawAddress ? (
-            <StyledButton onClick={handleSaveScore}>Save Your Score</StyledButton>
-          ) : (
-            <TonConnectButton />
+          {pageIndex > 0 && (
+            <StyledButtonSecondary onClick={handlePrevPage}>Previous</StyledButtonSecondary>
           )}
-          <StyledButtonSecondary onClick={onClose}>Close</StyledButtonSecondary>
         </ActionsContainer>
         <LeaderboardList>
-          {leaderboard
-            .slice(pageIndex * itemsPerPage, (pageIndex + 1) * itemsPerPage)
-            .map((entry, index) => (
-              <LeaderboardItem key={index}>
-                {formatAddress(entry.address, entry.userName, firstName)} - {entry.time.toFixed(2)} seconds
-              </LeaderboardItem>
-            ))}
-        </LeaderboardList>
-        <div>
-          {pageIndex > 0 && (
-            <button onClick={() => setPageIndex(pageIndex - 1)}>Previous</button>
+  {paginatedScores.map((entry, index) => (
+    <LeaderboardItem key={index}>
+      {pageIndex * itemsPerPage + index + 1}. {formatAddress(entry.address, entry.userName, firstName)} - {entry.time.toFixed(2)} seconds
+    </LeaderboardItem>
+  ))}
+</LeaderboardList>
+        <ActionsContainer>
+          {(pageIndex + 1) * itemsPerPage < topScores.length && (
+            <StyledButtonSecondary onClick={handleNextPage}>Next</StyledButtonSecondary>
           )}
-          {(pageIndex + 1) * itemsPerPage < leaderboard.length && (
-            <button onClick={() => setPageIndex(pageIndex + 1)}>Next</button>
-          )}
-        </div>
+        </ActionsContainer>
+        <ActionsContainer>
+          <StyledButton onClick={onClose}>Close</StyledButton>
+        </ActionsContainer>
       </LeaderboardContent>
       {showSaveScoreWindow && (
         <SaveScoreWindowContainer>
           <SaveScoreWindowContent>
-            <h2>Save Your Score</h2>
-            <p>Are you sure you want to save your score?</p>
-            <ActionsContainer>
-              <StyledButton onClick={handleSaveScoreConfirm}>Confirm</StyledButton>
-              <StyledButtonSecondary onClick={handleSaveScoreCancel}>Cancel</StyledButtonSecondary>
-            </ActionsContainer>
+            <p>Connect your wallet to set user Id and save your score:</p>
+            <TonConnectButton />
+            <StyledButton onClick={handleSaveScoreConfirm} style={{ marginTop: '10px' }}>Save Score</StyledButton>
+            <StyledButton onClick={handleCloseSaveScoreWindow} style={{ marginTop: '10px' }}>Close</StyledButton>
           </SaveScoreWindowContent>
         </SaveScoreWindowContainer>
       )}
